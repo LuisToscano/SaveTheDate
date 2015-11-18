0cae99901f66ae3c088bfa559b48211fa37e3801
End User <End User@EndUser-PC>
2015-11-17 22:51:22 -0500

    finished

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -5,8 +5,11 @@
  */
 
 $("document").ready(function(){
+    
+    var gallery = $('.gallery a').simpleLightbox();
+    
    $("div[id^='countdown']").countdown({
-    date: "30 January 2016 09:00:00", 
+    date: "30 January 2016 17:00:00", 
     format: "on"
   });
   
@@ -42,14 +45,15 @@ $("document").ready(function(){
        var id = $(this).attr("id");
        var numid = id.split("_")[1];
        $("div[id^='tab']").hide();
+       $("div[id='tab"+numid+"']").css("display", "flex");
+       $("div[id='tab"+numid+"']").hide();
        $("div[id='tab"+numid+"']").fadeIn(500);
     });
-  
-    $("#tab1").hide();
-    $("#tab3").hide();
     
     $(".party_specs p").click(function(){
        window.open("https://www.google.es/maps/place/Ruitoque+Golf+Country+Club/@7.0253055,-73.0846194,16z/data=!4m2!3m1!1s0x8e684759f471ff0f:0x1ecbf46fa03bcdf6");
     });
+    
+    $("#tab1").css("display", "flex");
 });
 
